fc4b08adabb6f4705cf628307a47aeb70ff70727
dadadon <davianberoni@yahoo.com>
2022-11-03 09:44:14 -0500

    Update file

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,60 +1,12 @@
 import calculate from "../components/logic/calculate";
 
-describe("calculate", () => {
-    it("returns an object", () => {
-        const result = calculate({}, "1");
-        expect(typeof result).toBe("object");
-    });
-    it("returns a total", () => {
-        const result = calculate({}, "1");
-        expect(result.total).not.toBeUndefined();
-    });
-    it("returns a next", () => {
-        const result = calculate({}, "1");
-        expect(result.next).not.toBeUndefined();
-    });
-    it("returns an operation", () => {
-        const result = calculate({}, "1");
-        expect(result.operation).not.toBeUndefined();
-    });
-    it("returns a total of 1", () => {
-        const result = calculate({}, "1");
-        expect(result.total).toBe("1");
-    });
-    it("returns a total of 2", () => {
-        const result = calculate({ total: "1" }, "1");
-        expect(result.total).toBe("2");
-    });
-    it("returns a total of 3", () => {
-        const result = calculate({ total: "2" }, "1");
-        expect(result.total).toBe("3");
-    });
-    it("returns a total of 4", () => {
-        const result = calculate({ total: "3" }, "1");
-        expect(result.total).toBe("4");
-    });
-    it("returns a total of 5", () => {
-        const result = calculate({ total: "4" }, "1");
-        expect(result.total).toBe("5");
-    });
-    it("returns a total of 6", () => {
-        const result = calculate({ total: "5" }, "1");
-        expect(result.total).toBe("6");
-    });
-    it("returns a total of 7", () => {
-        const result = calculate({ total: "6" }, "1");
-        expect(result.total).toBe("7");
-    });
-    it("returns a total of 8", () => {
-        const result = calculate({ total: "7" }, "1");
-        expect(result.total).toBe("8");
-    });
-    it("returns a total of 9", () => {
-        const result = calculate({ total: "8" }, "1");
-        expect(result.total).toBe("9");
-    });
-    it("returns a total of 10", () => {
-        const result = calculate({ total: "9" }, "1");
-        expect(result.total).toBe("10");
-    });
-});
+test("Test the calculator logic", () => {
+    const data = {
+      total: 2,
+      next: 4,
+      operation: null,
+    };
+    const value = { total: 4, next: null, operation: "+" };
+    expect(calculate(data, "+")).toEqual(value);
+  });
+  
