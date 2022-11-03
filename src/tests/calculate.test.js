27b2a94a87dc80b0d50fdd520e853c3eb32ce425
KareemWilson <kareemmagdywilson@gmail.com>
2022-11-03 21:18:29 +0200

    Use describe method to test calculate method

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,12 +1,25 @@
 import calculate from "../components/logic/calculate";
 
-test("Test the calculator logic", () => {
-    const data = {
-      total: 2,
-      next: 4,
-      operation: null,
-    };
-    const value = { total: 4, next: null, operation: "+" };
-    expect(calculate(data, "+")).toEqual(value);
+describe('Test addition', () => {
+  const obj = {
+    total: 2,
+    next: 2,
+    operation: '+',
+  };
+  const calc = calculate(obj, '=');
+  it('test addition', () => {
+    expect(calc.total).toBe('4');
   });
-  
+});
+
+describe('Test multiply', () => {
+  const obj = {
+    total: 6,
+    next: 2,
+    operation: 'x',
+  };
+  const calc = calculate(obj, '=');
+  it('test myltiply', () => {
+    expect(calc.total).toBe('12');
+  });
+});
